524fa3df30225bff13e313444057b96a9e6eebf1
Chumachenk0 <pavelchymachenko@gmail.com>
2023-02-23 22:30:56 +0200

    init: start brecketst task

--- src/index.js
+++ src/index.js
@@ -1,28 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-  const stack = [];
-  const opening_brackets = new Set();
-  const closing_brackets = new Set();
-
+  let isBalanced = true;
+while (isBalanced && str.length > 0) {
+  let foundMatch = false;
   for (let i = 0; i < bracketsConfig.length; i++) {
-    opening_brackets.add(bracketsConfig[i][0]);
-    closing_brackets.add(bracketsConfig[i][1]);
-  }
-
-  for (let i = 0; i < str.length; i++) {
-    const c = str[i];
-    if (opening_brackets.has(c)) {
-      stack.push(c);
-    } else if (closing_brackets.has(c)) {
-      if (stack.length === 0) {
-        return false;
-      }
-      const top = stack.pop();
-      if (!bracketsConfig.some((b) => b[0] === top && b[1] === c)) {
-        return false;
-      }
+    let bracket = bracketsConfig[i];
+    let pattern = bracket.join("");
+    if (str.includes(pattern)) {
+      str = str.replace(pattern, "");
+      foundMatch = true;
+      break;
     }
   }
-
-  return stack.length === 0;
+  if (!foundMatch) {
+    isBalanced = false;
+  }
 }
+return isBalanced && str.length === 0;
+}
+
+
+
